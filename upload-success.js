8a0a3b59693700cec89e76a3c24eda3741567d5e
Felix Kirchengast <mail@felixkirchengast.at>
2020-09-19 18:53:43 +0200

    up

--- upload-success.js
+++ upload-success.js
@@ -1,1 +1,28 @@
-console.info(`Run upload-success`);
+const github = require('@actions/github');
+const core = require('@actions/core');
+
+async function run() {
+  // This should be a token with access to your repository scoped in as a secret.
+  // The YML workflow will need to set myToken with the GitHub Secret Token
+  // myToken: ${{ secrets.GITHUB_TOKEN }}
+  // https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret
+  const myToken = core.getInput('myToken');
+
+  const octokit = github.getOctokit(myToken)
+
+  // You can also pass in additional options as a second parameter to getOctokit
+  // const octokit = github.getOctokit(myToken, {userAgent: "MyActionVersion1"});
+  console.info(JSON.stringify(octokit));
+
+  const { data: pullRequest } = await octokit.pulls.get({
+    owner: 'octokit',
+    repo: 'rest.js',
+    pull_number: 123,
+    mediaType: {
+      format: 'diff'
+    }
+  });
+  console.log(pullRequest);
+}
+
+run();
