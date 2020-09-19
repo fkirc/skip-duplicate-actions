e1fe1923776bf326916269f3c4ce3f3f100280a1
Felix Kirchengast <mail@felixkirchengast.at>
2020-09-19 18:59:36 +0200

    token

--- upload-success.js
+++ upload-success.js
@@ -4,11 +4,11 @@ const core = require('@actions/core');
 async function run() {
   // This should be a token with access to your repository scoped in as a secret.
   // The YML workflow will need to set myToken with the GitHub Secret Token
-  // myToken: ${{ secrets.GITHUB_TOKEN }}
+  // GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
   // https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret
-  const myToken = core.getInput('myToken');
+  const gitHubToken = core.getInput('github-token');
 
-  const octokit = github.getOctokit(myToken)
+  const octokit = github.getOctokit(gitHubToken)
 
   // You can also pass in additional options as a second parameter to getOctokit
   // const octokit = github.getOctokit(myToken, {userAgent: "MyActionVersion1"});
