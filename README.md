84b8476c42445a30e8bcfd1891b7fe1a105a7c28
Felix Kirchengast <mail@felixkirchengast.at>
2020-09-19 19:26:53 +0200

    local test

--- README.md
+++ README.md
@@ -16,6 +16,6 @@ The time we greeted you.
 
 ## Example usage
 
-uses: actions/hello-world-javascript-action@v1.1
+uses: actions/skip-duplicate-act@v1.1
 with:
   who-to-greet: 'Mona the Octocat'
--- index.js
+++ index.js
@@ -2,9 +2,6 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello ${nameToGreet}!`);
     const time = (new Date()).toTimeString();
     core.setOutput("time", time);
     // Get the JSON webhook payload for the event that triggered the workflow
