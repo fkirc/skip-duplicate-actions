d3d2ab364b921ace9da98404d2e05a6e65372565
Felix Kirchengast <mail@felixkirchengast.at>
2020-09-22 23:55:56 +0200

    print

--- src/index.ts
+++ src/index.ts
@@ -176,7 +176,7 @@ async function detectPathIgnore(context: WRunContext) {
 
 async function fetchCommitDetails(sha: string, context: WRunContext) {
   try {
-    console.log(Object.keys(context.octokit.keys)); // TODO: Remove
+    console.log(Object.keys(context.octokit)); // TODO: Remove
     const res = await context.octokit.commits.getCommit({
       owner: context.repoOwner,
       repo: context.repoName,
