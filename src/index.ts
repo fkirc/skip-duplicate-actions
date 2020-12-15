97f2cce5f290558d00254100ff40c8fa38c9a56b
Felix Kirchengast <mail@felixkirchengast.at>
2020-12-15 21:17:04 +0100

    Implement skipping of outdated runs

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,7 @@ type WorkflowRunConclusion = 'success' | 'failure' | 'neutral' | 'cancelled' | '
 const concurrentSkippingMap = {
   "always": null,
   "same_content": null,
+  "outdated_runs": null,
   "never": null,
 }
 function getConcurrentSkippingOptions(): string[] {
@@ -214,14 +215,20 @@ function detectConcurrentRuns(context: WRunContext) {
   if (context.concurrentSkipping === "always") {
     core.info(`Skip execution because another instance of the same workflow is already running in ${concurrentRuns[0].html_url}`);
     exitSuccess({ shouldSkip: true });
+  } else if (context.concurrentSkipping === "outdated_runs") {
+    const newerRun = concurrentRuns.find((run) => new Date(run.createdAt).getTime() > new Date(context.currentRun.createdAt).getTime());
+    if (newerRun) {
+      core.info(`Skip execution because a newer instance of the same workflow is running in ${newerRun.html_url}`);
+      exitSuccess({ shouldSkip: true });
+    }
+  } else if (context.concurrentSkipping === "same_content") {
+    const concurrentDuplicate = concurrentRuns.find((run) => run.treeHash === context.currentRun.treeHash);
+    if (concurrentDuplicate) {
+      core.info(`Skip execution because the exact same files are concurrently checked in ${concurrentDuplicate.html_url}`);
+      exitSuccess({ shouldSkip: true });
+    }
   }
-  const concurrentDuplicate = concurrentRuns.find((run) => run.treeHash === context.currentRun.treeHash);
-  if (concurrentDuplicate) {
-    core.info(`Skip execution because the exact same files are concurrently checked in ${concurrentDuplicate.html_url}`);
-    exitSuccess({ shouldSkip: true });
-  } else {
-    core.info(`Did not find any duplicate concurrent workflow-runs`);
-  }
+  core.info(`Did not find any skippable concurrent workflow-runs`);
 }
 
 async function backtracePathSkipping(context: WRunContext) {
