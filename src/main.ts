4c7b1999dc9f9e25ddf086c6474ba3c16df955a3
Pascal Jufer <pascal-jufer@bluewin.ch>
2021-11-26 10:51:23 +0100

    Introduce 'paths_filter' option along with new return values

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import * as github from '@actions/github'
 import {Endpoints} from '@octokit/types'
 import {GitHub} from '@actions/github/lib/utils'
 import micromatch from 'micromatch'
+import yaml from 'js-yaml'
 
 type ActionsGetWorkflowRunResponseData =
   Endpoints['GET /repos/{owner}/{repo}/actions/runs/{run_id}']['response']['data']
@@ -49,6 +50,13 @@ interface WorkflowRun {
 
 type WRunTrigger = 'pull_request' | 'push' | 'workflow_dispatch' | 'schedule'
 
+interface PathsFilterEntry {
+  paths_ignore: string[]
+  paths: string[]
+  backtracking: boolean | number
+}
+type PathsFilter = Record<string, PathsFilterEntry>
+
 interface WRunContext {
   repoOwner: string
   repoName: string
@@ -58,9 +66,19 @@ interface WRunContext {
   octokit: InstanceType<typeof GitHub>
   pathsIgnore: string[]
   paths: string[]
+  pathsFilter: PathsFilter
   doNotSkip: WRunTrigger[]
   concurrentSkipping: ConcurrentSkippingOption
 }
+
+interface PathsResultEntry {
+  should_skip: 'unknown' | boolean
+  backtrack_count: number
+  skipped_by?: WorkflowRun
+  matched_files?: string[]
+}
+
+type PathsResult = Record<string, PathsResultEntry>
 
 function parseWorkflowRun(run: ActionsGetWorkflowRunResponseData): WorkflowRun {
   const treeHash = run.head_commit?.tree_id
@@ -126,7 +144,7 @@ async function main(): Promise<void> {
       logFatal('Did not find runId')
     }
 
-    let context: WRunContext
+    let context: Readonly<WRunContext>
     try {
       const octokit = github.getOctokit(token)
       const {data: current_run} = await octokit.rest.actions.getWorkflowRun({
@@ -151,6 +169,7 @@ async function main(): Promise<void> {
         octokit,
         pathsIgnore: getStringArrayInput('paths_ignore'),
         paths: getStringArrayInput('paths'),
+        pathsFilter: getPathsFilterInput('paths_filter'),
         doNotSkip: getStringArrayInput('do_not_skip') as WRunTrigger[],
         concurrentSkipping: getConcurrentSkippingInput('concurrent_skipping')
       }
@@ -159,7 +178,10 @@ async function main(): Promise<void> {
         core.warning(e)
       }
       core.warning(`Failed to fetch the required workflow information`)
-      exitSuccess({shouldSkip: false})
+      exitSuccess({
+        shouldSkip: false,
+        reason: 'no_workflow_information'
+      })
     }
 
     const cancelOthers = getBooleanInput('cancel_others', false)
@@ -170,25 +192,62 @@ async function main(): Promise<void> {
       core.info(
         `Do not skip execution because the workflow was triggered with '${context.currentRun.event}'`
       )
-      exitSuccess({shouldSkip: false})
+      exitSuccess({
+        shouldSkip: false,
+        reason: 'do_not_skip'
+      })
     }
     const skipAfterSuccessfulDuplicates = getBooleanInput(
       'skip_after_successful_duplicate',
       true
     )
     if (skipAfterSuccessfulDuplicates) {
-      detectSuccessfulDuplicateRuns(context)
+      const successfulDuplicateRun = detectSuccessfulDuplicateRuns(context)
+      if (successfulDuplicateRun) {
+        core.info(
+          `Skip execution because the exact same files have been successfully checked in ${successfulDuplicateRun.html_url}`
+        )
+        exitSuccess({
+          shouldSkip: true,
+          reason: 'skip_after_successful_duplicate',
+          skippedBy: successfulDuplicateRun
+        })
+      }
     }
     if (context.concurrentSkipping !== 'never') {
-      detectConcurrentRuns(context)
+      const concurrentRun = detectConcurrentRuns(context)
+      if (concurrentRun) {
+        exitSuccess({
+          shouldSkip: true,
+          reason: 'concurrent_skipping',
+          skippedBy: concurrentRun
+        })
+      }
     }
-    if (context.paths.length >= 1 || context.pathsIgnore.length >= 1) {
-      await backtracePathSkipping(context)
+    if (
+      context.paths.length >= 1 ||
+      context.pathsIgnore.length >= 1 ||
+      Object.keys(context.pathsFilter).length >= 1
+    ) {
+      const {changedFiles, pathsResult} = await backtracePathSkipping(context)
+      exitSuccess({
+        shouldSkip:
+          pathsResult.global.should_skip === 'unknown'
+            ? false
+            : pathsResult.global.should_skip,
+        reason: 'paths',
+        skippedBy: pathsResult.global.skipped_by,
+        changedFiles,
+        pathsResult
+      })
     }
     core.info(
-      'Do not skip execution because we did not find a transferable run'
+      `Do not skip execution because we did not find a transferable run`
     )
-    exitSuccess({shouldSkip: false})
+    exitSuccess({
+      shouldSkip: false,
+      reason: 'no_transferable_run'
+    })
   } catch (e) {
     if (e instanceof Error) {
       core.error(e)
@@ -234,22 +293,19 @@ async function cancelWorkflowRun(
   }
 }
 
-function detectSuccessfulDuplicateRuns(context: WRunContext): void {
+function detectSuccessfulDuplicateRuns(
+  context: WRunContext
+): WorkflowRun | undefined {
   const duplicateRuns = context.olderRuns.filter(
     run => run.treeHash === context.currentRun.treeHash
   )
   const successfulDuplicate = duplicateRuns.find(run => {
     return run.status === 'completed' && run.conclusion === 'success'
   })
-  if (successfulDuplicate) {
-    core.info(
-      `Skip execution because the exact same files have been successfully checked in ${successfulDuplicate.html_url}`
-    )
-    exitSuccess({shouldSkip: true, skippedBy: successfulDuplicate})
-  }
+  return successfulDuplicate
 }
 
-function detectConcurrentRuns(context: WRunContext): void {
+function detectConcurrentRuns(context: WRunContext): WorkflowRun | undefined {
   const concurrentRuns: WorkflowRun[] = context.allRuns.filter(run => {
     if (run.status === 'completed') {
       return false
@@ -259,6 +315,7 @@ function detectConcurrentRuns(context: WRunContext): void {
     }
     return true
   })
+
   if (!concurrentRuns.length) {
     core.info(`Did not find any concurrent workflow-runs`)
     return
@@ -267,7 +324,7 @@ function detectConcurrentRuns(context: WRunContext): void {
     core.info(
       `Skip execution because another instance of the same workflow is already running in ${concurrentRuns[0].html_url}`
     )
-    exitSuccess({shouldSkip: true, skippedBy: concurrentRuns[0]})
+    return concurrentRuns[0]
   } else if (context.concurrentSkipping === 'outdated_runs') {
     const newerRun = concurrentRuns.find(
       run =>
@@ -278,7 +335,7 @@ function detectConcurrentRuns(context: WRunContext): void {
       core.info(
         `Skip execution because a newer instance of the same workflow is running in ${newerRun.html_url}`
       )
-      exitSuccess({shouldSkip: true, skippedBy: newerRun})
+      return newerRun
     }
   } else if (context.concurrentSkipping === 'same_content') {
     const concurrentDuplicate = concurrentRuns.find(
@@ -288,7 +345,7 @@ function detectConcurrentRuns(context: WRunContext): void {
       core.info(
         `Skip execution because the exact same files are concurrently checked in ${concurrentDuplicate.html_url}`
       )
-      exitSuccess({shouldSkip: true, skippedBy: concurrentDuplicate})
+      return concurrentDuplicate
     }
   } else if (context.concurrentSkipping === 'same_content_newer') {
     const concurrentIsOlder = concurrentRuns.find(
@@ -300,125 +357,162 @@ function detectConcurrentRuns(context: WRunContext): void {
       core.info(
         `Skip execution because the exact same files are concurrently checked in older ${concurrentIsOlder.html_url}`
       )
-      exitSuccess({shouldSkip: true, skippedBy: concurrentIsOlder})
+      return concurrentIsOlder
     }
   }
-  core.info(`Did not find any skippable concurrent workflow-runs`)
+  core.info(`Did not find any concurrent workflow-runs that justify skipping`)
 }
 
-async function backtracePathSkipping(context: WRunContext): Promise<void> {
+async function backtracePathSkipping(
+  context: WRunContext
+): Promise<{changedFiles: string[][]; pathsResult: PathsResult}> {
   let commit: ReposGetCommitResponseData | null
   let iterSha: string | null = context.currentRun.commitHash
   let distanceToHEAD = 0
+  const allChangedFiles: string[][] = []
+
+  const pathsFilter: PathsFilter = {
+    ...context.pathsFilter,
+    global: {
+      paths: context.paths,
+      paths_ignore: context.pathsIgnore,
+      backtracking: true
+    }
+  }
+
+  const pathsResult: PathsResult = {}
+  for (const name of Object.keys(pathsFilter)) {
+    pathsResult[name] = {should_skip: 'unknown', backtrack_count: 0}
+  }
+
   do {
     commit = await fetchCommitDetails(iterSha, context)
     if (!commit) {
-      return
+      break
     }
     iterSha = commit.parents?.length ? commit.parents[0]?.sha : null
+    const changedFiles = commit.files
+      ? (commit.files
+          .map(f => f.filename)
+          .filter(f => typeof f === 'string') as string[])
+      : []
+    allChangedFiles.push(changedFiles)
 
-    if (distanceToHEAD >= 1) {
-      exitIfSuccessfulRunExists(commit, context)
+    const successfulRun =
+      (distanceToHEAD >= 1 &&
+        findSuccessfulRun(commit.commit.tree.sha, context.olderRuns)) ||
+      undefined
+
+    for (const [name, values] of Object.entries(pathsResult)) {
+      // Only process paths where status is not determined yet.
+      if (values.should_skip !== 'unknown') continue
+
+      // Skip if paths were ignorable or skippable until now and there is a successful run on this commit.
+      if (successfulRun) {
+        pathsResult[name].should_skip = true
+        pathsResult[name].skipped_by = successfulRun
+        pathsResult[name].backtrack_count = distanceToHEAD
+        core.info(
+          `Skip '${name}' because all changes since ${successfulRun.html_url} are in ignored or skipped paths`
+        )
+        continue
+      }
+
+      if (
+        (pathsFilter[name].backtracking === false && distanceToHEAD === 1) ||
+        pathsFilter[name].backtracking === distanceToHEAD
+      ) {
+        pathsResult[name].should_skip = true
+        pathsResult[name].backtrack_count = distanceToHEAD
+        core.info(
+          `Aborted backtracking for '${name}' since the defined limit has been reached`
+        )
+        core.info(
+          `Stop backtracking for '${name}' because the defined limit has been reached`
+        )
+        continue
+      }
+
+      // Ignorable if all changed files match against ignored paths.
+      if (isCommitPathsIgnored(changedFiles, pathsFilter[name].paths_ignore)) {
+        core.info(
+          `Commit ${commit.html_url} is path-ignored for '${name}': All of '${changedFiles}' match against patterns '${pathsFilter[name].paths_ignore}'`
+        )
+        continue
+      }
+
+      // Skippable if none of the changed files matches against paths.
+      if (pathsFilter[name].paths.length >= 1) {
+        const matches = getCommitPathsMatches(
+          changedFiles,
+          pathsFilter[name].paths
+        )
+        if (matches.length === 0) {
+          core.info(
+            `Commit ${commit.html_url} is path-skipped for '${name}': None of '${changedFiles}' matches against patterns '${pathsFilter[name].paths}'`
+          )
+          continue
+        } else {
+          pathsResult[name].matched_files = matches
+        }
+      }
+
+      // Not ignorable or skippable
+      pathsResult[name].should_skip = false
+      pathsResult[name].backtrack_count = distanceToHEAD
+      core.info(
+        `Stop backtracking for '${name}' at commit ${commit.html_url} because '${changedFiles}' are not skippable against paths '${pathsFilter[name].paths}' or paths_ignore '${pathsFilter[name].paths_ignore}'`
+      )
     }
 
+    // Should be never reached in practice; we expect that this loop aborts after 1-3 iterations.
     if (distanceToHEAD++ >= 50) {
-      // Should be never reached in practice; we expect that this loop aborts after 1-3 iterations.
       core.warning(
         'Aborted commit-backtracing due to bad performance - Did you push an excessive number of ignored-path-commits?'
       )
-      return
+      break
     }
-  } while (isCommitSkippable(commit, context))
+  } while (
+    Object.keys(pathsResult).some(
+      path => pathsResult[path].should_skip === 'unknown'
+    )
+  )
+
+  return {changedFiles: allChangedFiles, pathsResult}
 }
 
-function exitIfSuccessfulRunExists(
-  commit: ReposGetCommitResponseData,
-  context: WRunContext
-): void {
-  const treeHash = commit.commit.tree.sha
-  const matchingRuns = context.olderRuns.filter(
-    run => run.treeHash === treeHash
-  )
+function findSuccessfulRun(
+  treeHash: string,
+  olderRuns: WorkflowRun[]
+): WorkflowRun | undefined {
+  const matchingRuns = olderRuns.filter(run => run.treeHash === treeHash)
   const successfulRun = matchingRuns.find(run => {
     return run.status === 'completed' && run.conclusion === 'success'
   })
-  if (successfulRun) {
-    core.info(
-      `Skip execution because all changes since ${successfulRun.html_url} are in ignored or skipped paths`
-    )
-    exitSuccess({shouldSkip: true, skippedBy: successfulRun})
-  }
-}
-
-function isCommitSkippable(
-  commit: ReposGetCommitResponseData,
-  context: WRunContext
-): boolean {
-  const changedFiles = commit.files ? commit.files.map(f => f.filename) : []
-  if (isCommitPathIgnored(commit, context)) {
-    core.info(
-      `Commit ${commit.html_url} is path-ignored: All of '${changedFiles}' match against patterns '${context.pathsIgnore}'`
-    )
-    return true
-  }
-  if (isCommitPathSkipped(commit, context)) {
-    core.info(
-      `Commit ${commit.html_url} is path-skipped: None of '${changedFiles}' matches against patterns '${context.paths}'`
-    )
-    return true
-  }
-  core.info(
-    `Stop backtracking at commit ${commit.html_url} because '${changedFiles}' are not skippable against paths '${context.paths}' or paths_ignore '${context.pathsIgnore}'`
-  )
-  return false
+  return successfulRun
 }
 
 const globOptions = {
-  dot: true // Match dotfiles. Otherwise dotfiles are ignored unless a . is explicitly defined in the pattern.
+  dot: true // Match dotfiles. Otherwise dotfiles are ignored unless a "." is explicitly defined in the pattern.
 }
 
-function isCommitPathIgnored(
-  commit: ReposGetCommitResponseData,
-  context: WRunContext
+function isCommitPathsIgnored(
+  changedFiles: string[],
+  pathsIgnore: string[]
 ): boolean {
-  if (!context.pathsIgnore.length) {
+  if (pathsIgnore.length === 0) {
     return false
   }
-  if (commit.files) {
-    // Skip if all changed files match against pathsIgnore.
-    const changedFiles = commit.files.map(f => f.filename ?? '')
-    const notIgnoredPaths = micromatch.not(
-      changedFiles,
-      context.pathsIgnore,
-      globOptions
-    )
-    return notIgnoredPaths.length === 0
-  } else {
-    // Skip if no files have changed.
-    return true
-  }
+  const notIgnoredPaths = micromatch.not(changedFiles, pathsIgnore, globOptions)
+  return notIgnoredPaths.length === 0
 }
 
-function isCommitPathSkipped(
-  commit: ReposGetCommitResponseData,
-  context: WRunContext
-): boolean {
-  if (!context.paths.length) {
-    return false
-  }
-  if (commit.files) {
-    // Skip if none of the changed files matches against context.paths.
-    const changedFiles = commit.files.map(f => f.filename ?? '')
-    const matchExists = micromatch.some(
-      changedFiles,
-      context.paths,
-      globOptions
-    )
-    return !matchExists
-  } else {
-    // Skip if no files have changed.
-    return true
-  }
+function getCommitPathsMatches(
+  changedFiles: string[],
+  paths: string[]
+): string[] {
+  const matches = micromatch(changedFiles, paths, globOptions)
+  return matches
 }
 
 async function fetchCommitDetails(
@@ -446,10 +540,16 @@ async function fetchCommitDetails(
 
 function exitSuccess(args: {
   shouldSkip: boolean
+  reason: string
   skippedBy?: WorkflowRun
+  changedFiles?: string[][]
+  pathsResult?: PathsResult
 }): never {
   core.setOutput('should_skip', args.shouldSkip)
-  core.setOutput('skipped_by', args.skippedBy)
+  core.setOutput('reason', args.reason)
+  core.setOutput('skipped_by', args.skippedBy || {})
+  core.setOutput('changed_files', args.changedFiles || [])
+  core.setOutput('paths_result', args.pathsResult || {})
   return process.exit(0)
 }
 
@@ -504,6 +604,33 @@ function getStringArrayInput(name: string): string[] {
       core.error(e)
     }
     logFatal(`Input '${rawInput}' is not a valid JSON`)
+  }
+}
+
+function getPathsFilterInput(name: string): PathsFilter {
+  const rawInput = core.getInput(name, {required: false})
+  if (!rawInput) {
+    return {}
+  }
+  try {
+    const input = yaml.load(rawInput)
+    // Assign default values
+    const pathsFilter: PathsFilter = {}
+    for (const [key, value] of Object.entries(
+      input as Record<string, Partial<PathsFilterEntry>>
+    )) {
+      pathsFilter[key] = {
+        paths: value.paths || [],
+        paths_ignore: value.paths_ignore || [],
+        backtracking: value.backtracking == null ? true : value.backtracking
+      }
+    }
+    return pathsFilter
+  } catch (e) {
+    if (e instanceof Error || typeof e === 'string') {
+      core.error(e)
+    }
+    logFatal(`Input '${rawInput}' is invalid`)
   }
 }
 
