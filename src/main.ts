4fcebfa2cbd1a6ed8bcded53754626fa44bbf283
Pascal Jufer <pascal-jufer@bluewin.ch>
2022-09-22 08:43:50 +0200

    Use one for loop to process all runs

--- src/main.ts
+++ src/main.ts
@@ -450,46 +450,51 @@ async function main(): Promise<void> {
   const octokit = github.getOctokit(token)
 
   // Get and parse the current workflow run.
-  const {data: workflowRun} = await octokit.rest.actions.getWorkflowRun({
+  const {data: apiCurrentRun} = await octokit.rest.actions.getWorkflowRun({
     ...repo,
     run_id: github.context.runId
   })
-  const treeHash = workflowRun.head_commit?.tree_id
-  if (!treeHash) {
+  const currentTreeHash = apiCurrentRun.head_commit?.tree_id
+  if (!currentTreeHash) {
     exitFail(`
-        Could not find the tree hash of run ${workflowRun.id} (Workflow ID: ${workflowRun.workflow_id},
-        Name: ${workflowRun.name}, Head Branch: ${workflowRun.head_branch}, Head SHA: ${workflowRun.head_sha}).
+        Could not find the tree hash of run ${apiCurrentRun.id} (Workflow ID: ${apiCurrentRun.workflow_id},
+        Name: ${apiCurrentRun.name}, Head Branch: ${apiCurrentRun.head_branch}, Head SHA: ${apiCurrentRun.head_sha}).
         This might be a run associated with a headless or removed commit.
       `)
   }
-  const currentRun = mapWorkflowRun(workflowRun, treeHash)
+  const currentRun = mapWorkflowRun(apiCurrentRun, currentTreeHash)
 
   // Fetch list of runs for current workflow.
   const {
-    data: {workflow_runs: workflowRuns}
+    data: {workflow_runs: apiAllRuns}
   } = await octokit.rest.actions.listWorkflowRuns({
     ...repo,
     workflow_id: currentRun.workflowId,
     per_page: 100
   })
 
+  // List with all workflow runs.
+  const allRuns = []
+  // List with older workflow runs only (used to prevent some nasty race conditions and edge cases).
+  const olderRuns = []
+
   // Check and map all runs.
-  const allRuns = workflowRuns.reduce((result: WorkflowRun[], run) => {
+  for (const run of apiAllRuns) {
     // Filter out current run and runs that lack 'head_commit' (most likely runs associated with a headless or removed commit).
     // See https://github.com/fkirc/skip-duplicate-actions/pull/178.
     if (run.id !== currentRun.id && run.head_commit) {
-      result.push(mapWorkflowRun(run, run.head_commit.tree_id))
+      const mappedRun = mapWorkflowRun(run, run.head_commit.tree_id)
+      // Add to list of all runs.
+      allRuns.push(mappedRun)
+      // Check if run can be added to list of older runs.
+      if (
+        new Date(mappedRun.createdAt).getTime() <
+        new Date(currentRun.createdAt).getTime()
+      ) {
+        olderRuns.push(mappedRun)
+      }
     }
-    return result
-  }, [])
-
-  // List with older workflow runs only (used to prevent some nasty race conditions and edge cases).
-  const olderRuns = allRuns.filter(run => {
-    return (
-      new Date(run.createdAt).getTime() <
-      new Date(currentRun.createdAt).getTime()
-    )
-  })
+  }
 
   const skipDuplicateActions = new SkipDuplicateActions(inputs, {
     repo,
